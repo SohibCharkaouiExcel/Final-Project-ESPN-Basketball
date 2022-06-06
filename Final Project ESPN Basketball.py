fe20a2c8325944417229f08b598e69b28cccba1a
SohibCharkaoui <sohib.charkaoui@excelacademy.org>
2022-06-06 09:05:53 -0400

    Final Project ESPN Basketball.py

--- Final Project ESPN Basketball.py
+++ Final Project ESPN Basketball.py
@@ -41,6 +41,62 @@ teams.remove('Milwaukee Bucks')
 teams.remove('Memphis Grizzlies')
 teams.remove('Minnesota Timberwovles')
 teams.remove('Philadelphia 76ers')
+    
+
+answer = str(input("Which team are you rooting for?"))
+proceed = "Golden State Warriors" or "golden state warriors"
+proceed1 = "Memphis Grizzlies" or "memphis grizzlies"
+proceed2 = "Denver Nuggets" or "denver nuggets"
+proceed3 = "New Orleans Pelicans" or "new orleans pelicans"
+proceed4 = "Minnesota Timberwolves" or "minnesota timberwolves"
+proceed5 = "Brooklyn Nets" or "brooklyn nets"
+proceed6 = "Boston Celtics" or "boston celtics"
+proceed7 = "Phoenix Suns" or "phoenix suns"
+proceed8 = "Dallas Mavericks" or "dallas mavericks"
+proceed9 = "Philadelphia 76ers" or "philadelphia 76ers"
+proceed10 = "Miami Heat" or "miami heat"
+proceed11 = "Toronto Raptors" or "toronto raptors"
+proceed12 = "Milwaukee Bucks" or "milwaukee bucks"
+proceed13 = "Utah Jazz" or "utah jazz"
+proceed14 = "Atlanta Hawks" or "atlanta hawks"
+proceed15 = "Chicago Bulls" or "chicago bulls"
+
+
+
+if answer == proceed:
+ print("your team has 114 points")
+elif answer == proceed1:
+    print("your team has 112 points")
+elif answer == proceed2:
+    print("your team has 110 points")
+elif answer == proceed3:
+    print("your team has 109 points")
+elif answer == proceed4:
+    print("your team has 109 points")
+elif answer == proceed5:
+    print("your team has 109 points")
+elif answer == proceed6:
+    print("your team has 107 points")
+elif answer == proceed7:
+    print("your team has 107 points")
+elif answer == proceed8:
+    print("your team has 106 points")
+elif answer == proceed9:
+    print("your team has 104 points")
+elif answer == proceed10:
+    print("your team has 104 points")
+elif answer == proceed11:
+    print("your team has 103 points")
+elif answer == proceed12:
+    print("your team has 102 points")
+elif answer == proceed13:
+    print("your team has 99 points")
+elif answer == proceed14:
+    print("your team has 97 points")
+elif answer == proceed15:
+    print("your team has 95 points")
+else:
+ print("thats not a basketball team goofy fella")
 
 
     
